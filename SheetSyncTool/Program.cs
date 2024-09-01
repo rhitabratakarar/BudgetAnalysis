4806a2a3fc35c3bc61d4aef98121148b8e51633f
Rhitabrata Karar <kararrhitabrata@gmail.com>
2024-09-01 21:14:45 +0530

    added user secrets support to SheetSyncTool

--- SheetSyncTool/Program.cs
+++ SheetSyncTool/Program.cs
@@ -1,1 +1,9 @@
-﻿// This project detects changes from the Google Sheets and inserts them into database
+﻿using Microsoft.Extensions.Configuration;
+using System.Reflection;
+
+IConfiguration configuration = new ConfigurationBuilder().AddUserSecrets(Assembly.GetExecutingAssembly()).Build();
+
+string? API_KEY = configuration["SHEETS_ONLY_API_KEY"];
+
+if (API_KEY != null)
+    Console.WriteLine(API_KEY);
