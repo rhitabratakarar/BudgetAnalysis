a948248cee1ce00d8692850af4106e8286599861
Rhitabrata Karar <kararrhitabrata@gmail.com>
2024-09-02 08:19:05 +0530

    added exception handling

--- SheetSyncTool/Program.cs
+++ SheetSyncTool/Program.cs
@@ -11,13 +11,23 @@ namespace SheetSyncTool
                                             .AddUserSecrets(Assembly.GetExecutingAssembly())
                                             .Build();
 
+            #region api_key_testing
             // string? API_KEY = configuration["SHEETS_ONLY_API_KEY"];
 
             // if (API_KEY != null)
             //    Console.WriteLine(API_KEY);
+            #endregion
 
-            ISheetSyncer sheetSyncer = new SheetSyncer(configuration);
-            sheetSyncer.Sync();
+            try
+            {
+                ISheetSyncer sheetSyncer = new SheetSyncer(configuration);
+                sheetSyncer.Sync();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("An exception has occured: " + e.Message);
+                Console.WriteLine("Look into the stack Trace: " + e.StackTrace);
+            }
         }
     }
 }
--- SheetSyncTool/SheetSyncer.cs
+++ SheetSyncTool/SheetSyncer.cs
@@ -5,10 +5,15 @@ namespace SheetSyncTool
     internal class SheetSyncer : ISheetSyncer
     {
         private readonly IConfiguration configuration;
+        private readonly string? API_KEY;
 
         public SheetSyncer(IConfiguration configuration)
         {
             this.configuration = configuration;
+            this.API_KEY = this.configuration["SHEETS_ONLY_API_KEY"];
+
+            if (this.API_KEY == null)
+                throw new Exception("api key is missing in user secrets.");
         }
 
         public void Sync()
