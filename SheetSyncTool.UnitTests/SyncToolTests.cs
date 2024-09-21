c06d09b91301cc816c2f8bb03835042097f7018e
Rhitabrata Karar <kararrhitabrata@gmail.com>
2024-09-22 00:17:37 +0530

    Added unit testing project

---
+++ SheetSyncTool.UnitTests/SyncToolTests.cs
@@ -0,0 +1,12 @@
+namespace SheetSyncTool.UnitTests
+{
+    [TestClass]
+    public class SyncToolTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
