fd12193a2b57b68185d6cf84ab27d680e553f98a
Rhitabrata Karar <kararrhitabrata@gmail.com>
2024-10-08 09:01:31 +0530

    Update SheetSyncer.cs
    Code restructuring. Broken a big function into smaller chunks.

--- SheetSyncTool/SheetSyncer.cs
+++ SheetSyncTool/SheetSyncer.cs
@@ -37,15 +37,26 @@ namespace SheetSyncTool
             string spreadSheetId = this.configuration["SheetID"]!;
             string rangeInSheet = this.configuration["SheetName"]! + "!" + this.configuration["SheetColumnRange"]!;
 
+            UserCredential credential = await GenerateAndGetValidCredential(CLIENT_SECRETS_FILE);
+
+            SheetsService service = GetSpreadSheetService(credential);
+
+            IList<IList<object>> contentToSend = await GetContentToSendFromSpreadSheet(spreadSheetId, rangeInSheet, service);
+
+            IDictionary<string, IList<IList<object>>> marshalledContent = GetMarshalledContent(contentToSend);
+
+            await SaveToDatabase(marshalledContent);
+        }
+
+        private async Task<UserCredential> GenerateAndGetValidCredential(string clientSecretFile)
+        {
             UserCredential? credential = null;
 
-
-            #region GENERATE VALID CREDENTIAL.
-            if (CLIENT_SECRETS_FILE != null)
+            if (clientSecretFile != null)
             {
                 try
                 {
-                    using (var stream = new FileStream(CLIENT_SECRETS_FILE, FileMode.Open, FileAccess.Read))
+                    using (var stream = new FileStream(clientSecretFile, FileMode.Open, FileAccess.Read))
                     {
                         credential = await GoogleWebAuthorizationBroker.AuthorizeAsync(
                                 GoogleClientSecrets.FromStream(stream).Secrets,
@@ -63,65 +74,15 @@ namespace SheetSyncTool
                     throw new Exception("❌ ERROR: " + e.Message);
                 }
             }
-            #endregion
 
-
-            #region CREATE SPREADSHEET'S SERVICE.
-            SheetsService service = new SheetsService(new Google.Apis.Services.BaseClientService.Initializer()
-            {
-                HttpClientInitializer = credential,
-                ApplicationName = this.configuration["ApplicationName"]!
-            });
-            #endregion
-
-
-            #region EXECUTE SPREADSHEET OPERATIONS.
-
-            IList<IList<object>> contentToSend = new List<IList<object>>();
-
-            if (service != null && spreadSheetId != null && spreadSheetId != "")
-            {
-                try
-                {
-                    SpreadsheetsResource.ValuesResource.GetRequest sheetsRequest = service.Spreadsheets.Values.Get(spreadSheetId, rangeInSheet);
-                    ValueRange values = await sheetsRequest.ExecuteAsync();
-
-                    contentToSend = values.Values;
-                }
-                catch (AggregateException exs)
-                {
-                    foreach (Exception e in exs.InnerExceptions)
-                    {
-                        Console.WriteLine("❌ ERROR: " + e.Message);
-                    }
-                    return;
-                }
-            }
-            #endregion
-
-
-            #region CONTENT MARSHALLING
-
-            IDictionary<string, IList<IList<object>>> marshalledContent = new Dictionary<string, IList<IList<object>>>();
-
-            if (contentToSend != null)
-            {
-                contentToSend.Insert(0, new List<object>() { "Year", this.configuration["SheetYear"]! });
-                contentToSend.Insert(1, new List<object>() { "Month", this.configuration["SheetName"]! });
-                contentToSend.Insert(2, new List<object>() { "SheetColumnRange", this.configuration["SheetColumnRange"]! });
-
-                marshalledContent.Add("InsertionData", contentToSend);
-            }
+            if (credential != null)
+                return credential;
             else
-            {
-                throw new Exception("❌ Received Empty content from GCloud.");
-            }
+                throw new Exception("Find NULL for credential object.");
+        }
 
-            #endregion
-
-
-            #region INSERT INTO DATABASE.
-
+        private async Task SaveToDatabase(IDictionary<string, IList<IList<object>>>? marshalledContent)
+        {
             if (marshalledContent != null)
             {
                 HttpClient client = new HttpClient();
@@ -141,9 +102,74 @@ namespace SheetSyncTool
             {
                 throw new Exception("Content is missing 😵‍💫. Debug to find it out 😓");
             }
-            #endregion
+        }
 
-            return;
+        private SheetsService GetSpreadSheetService(UserCredential credential)
+        {
+            SheetsService service;
+            try
+            {
+                service = new SheetsService(new Google.Apis.Services.BaseClientService.Initializer()
+                {
+                    HttpClientInitializer = credential,
+                    ApplicationName = this.configuration["ApplicationName"]!
+                });
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+
+            return service;
+        }
+
+        private async Task<IList<IList<object>>> GetContentToSendFromSpreadSheet(string spreadSheetId, string rangeInSheet, SheetsService service)
+        {
+            IList<IList<object>>? contentToSend = null;
+
+            if (spreadSheetId != "")
+            {
+                try
+                {
+                    SpreadsheetsResource.ValuesResource.GetRequest sheetsRequest = service.Spreadsheets.Values.Get(spreadSheetId, rangeInSheet);
+                    ValueRange values = await sheetsRequest.ExecuteAsync();
+
+                    contentToSend = values.Values;
+                }
+                catch (AggregateException exs)
+                {
+                    foreach (Exception e in exs.InnerExceptions)
+                    {
+                        Console.WriteLine("❌ ERROR: " + e.Message);
+                    }
+                    throw;
+                }
+            }
+
+            if (contentToSend != null)
+                return contentToSend;
+            else
+                throw new Exception("Received NULL while getting content.");
+        }
+
+        public IDictionary<string, IList<IList<object>>> GetMarshalledContent(IList<IList<object>> content)
+        {
+            IDictionary<string, IList<IList<object>>> marshalledContent = new Dictionary<string, IList<IList<object>>>();
+
+            if (content != null)
+            {
+                content.Insert(0, new List<object>() { "Year", this.configuration["SheetYear"]! });
+                content.Insert(1, new List<object>() { "Month", this.configuration["SheetName"]! });
+                content.Insert(2, new List<object>() { "SheetColumnRange", this.configuration["SheetColumnRange"]! });
+
+                marshalledContent.Add("InsertionData", content);
+            }
+            else
+            {
+                throw new Exception("❌ Received Empty content from GCloud.");
+            }
+
+            return marshalledContent;
         }
     }
 }
