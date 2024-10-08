8b98733e192c2245886bba59d263bc1d963b5b2c
Rhitabrata Karar <kararrhitabrata@gmail.com>
2024-10-08 14:37:53 +0530

    Create SheetExpensesMarshalledData.cs
    New class

---
+++ BudgetAnalysisDbApi/Classes/SheetExpensesMarshalledData.cs
@@ -0,0 +1,12 @@
+﻿namespace BudgetAnalysisDbApi.Classes
+{
+    public class SheetExpensesMarshalledData
+    {
+        public string Year { get; set; }
+        public string Month { get; set; }
+        public string SheetColumnRange { get; set; }
+        public IList<string> SheetHeaders { get; set; }
+        public IDictionary<string, string> MandatoryExpenses { get; set; }
+        public IDictionary<string, string> OptionalExpenses { get; set; }
+    }
+}
