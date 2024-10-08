7ff789790836bc02535f777b9874f71be2daa6db
Rhitabrata Karar <kararrhitabrata@gmail.com>
2024-10-08 17:37:03 +0530

    Create BudgetAnalysisDbContext.cs
    dbcontext to connect with database

---
+++ BudgetAnalysisDbApi/Database/BudgetAnalysisDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BudgetAnalysisDbApi.Database
+{
+    public class BudgetAnalysisDbContext: DbContext
+    {
+        private readonly IConfiguration _configuration;
+
+        public BudgetAnalysisDbContext(IConfiguration configuration)
+        {
+            this._configuration = configuration;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer(this._configuration.GetConnectionString("BudgetAnalysis"));
+        }
+    }
+}
