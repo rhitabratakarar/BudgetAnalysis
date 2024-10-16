be0b62279089aca0286ab9c640a025fc91a32f5f
Rhitabrata Karar <kararrhitabrata@gmail.com>
2024-10-16 09:14:08 +0530

    added relevant comments

--- BudgetAnalysisDbApi/Database/DbService.cs
+++ BudgetAnalysisDbApi/Database/DbService.cs
@@ -26,25 +26,33 @@ namespace BudgetAnalysisDbApi.Database
 
             try
             {
+                // check whether a year is existing from marshalled data.
                 int yearToCheck = Convert.ToInt32(marshalledData.Year);
                 Year? existingYear = await this.dbContext.Years.FirstOrDefaultAsync(y => y.YearCode == yearToCheck);
 
                 if (existingYear == null)
                 {
-                    // insert the newly added year into the database.
+                    // insert the newly added year into the database if it is not present.
                     await this.dbContext.Years.AddAsync(new Year() { YearCode = yearToCheck });
                 }
 
+                // check whether a month is existing from marshalled data.
                 string monthToCheck = marshalledData.Month.Trim();
                 Month? existingMonth = await this.dbContext.Months.FirstOrDefaultAsync(m => m.MonthName == monthToCheck);
 
                 if (existingMonth == null)
                 {
-                    // insert the newly added month into database.
+                    // insert the newly added month into the database if it is not present.
                     await this.dbContext.Months.AddAsync(new Month() { MonthName = monthToCheck });
                 }
 
-                // todo: proceed with inserting in expenses
+        // todo: proceed with inserting in expenses
+
+                // get the month id
+
+                // get the year id
+
+                // insert into expenses table. No duplicates should be present, need to update every expense with its latest value.
 
                 await this.dbContext.SaveChangesAsync();
                 status = true;
