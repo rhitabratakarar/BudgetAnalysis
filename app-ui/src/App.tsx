35960251a2465dee2128289133473694027a9cce
Rhitabrata Karar <kararrhitabrata@gmail.com>
2024-10-20 10:30:11 +0530

    Formatting

--- app-ui/src/App.tsx
+++ app-ui/src/App.tsx
@@ -1,12 +1,18 @@
-import './App.css';
-import Navbar from './components/Navbar';
+import "./App.css";
+import Navbar from "./components/Navbar";
+import * as React from "react";
+import Currentpage from "./Utils/CurrentPage";
 
 function App() {
-	return (
-		<div className="App">
-			<Navbar />
-		</div>
-	);
+  const [currentPage, setCurrentPage] = React.useState<Currentpage>(
+    Currentpage.Home
+  );
+
+  return (
+    <div className="App">
+      <Navbar currentPage={currentPage} setCurrentPage={setCurrentPage} />
+    </div>
+  );
 }
 
 export default App;
