5a71d5620161f53e5cb4d4eedefd18f492b5d270
Rhitabrata Karar <kararrhitabrata@gmail.com>
2024-10-22 09:59:06 +0530

    Update Card.tsx
    Added navigable button and reduced margin of card text

--- app-ui/src/components/Common/Card.tsx
+++ app-ui/src/components/Common/Card.tsx
@@ -3,10 +3,11 @@ export default function Card() {
     <div className="card shadow-sm mx-2 flex-shrink-0 mb-3 flex-grow-1" style={{ width: "18rem" }}>
       <img src="..." className="card-img-top" alt="" />
       <div className="card-body">
-        <p className="card-text">
+        <p className="card-text mb-2">
           Some quick example text to build on the card title and make up the
           bulk of the card's content.
         </p>
+        <button className="btn btn-primary">View Details</button>
       </div>
     </div>
   );
