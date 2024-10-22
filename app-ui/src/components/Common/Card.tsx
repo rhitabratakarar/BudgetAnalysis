50a8056f4f7c661d95f863e2892b98d3258a8135
Rhitabrata Karar <kararrhitabrata@gmail.com>
2024-10-22 09:46:18 +0530

    added card component
    Added card component

---
+++ app-ui/src/components/Common/Card.tsx
@@ -0,0 +1,13 @@
+export default function Card() {
+  return (
+    <div className="card shadow-sm mx-2 flex-shrink-0 mb-3 flex-grow-1" style={{ width: "18rem" }}>
+      <img src="..." className="card-img-top" alt="" />
+      <div className="card-body">
+        <p className="card-text">
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </p>
+      </div>
+    </div>
+  );
+}
