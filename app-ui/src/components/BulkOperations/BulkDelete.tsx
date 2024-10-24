56932e7ac247e6aec5dbfe6ce3bf042ffe1346a9
Rhitabrata Karar <kararrhitabrata@gmail.com>
2024-10-24 09:42:48 +0530

    Update BulkDelete.tsx
    added basic layout

--- app-ui/src/components/BulkOperations/BulkDelete.tsx
+++ app-ui/src/components/BulkOperations/BulkDelete.tsx
@@ -1,3 +1,24 @@
 export default function BulkDelete() {
-    return <div>Bulk Delete Page</div>;
+  return (
+    <div className="container-fluid">
+      <h6 className="mb-4">
+        <strong>Note: </strong>This operation is irreversible.
+      </h6>
+      <select className="form-select mb-3 w-25" aria-label="Year Select">
+        <option selected>Select Year</option>
+        <option value="1">One</option>
+        <option value="2">Two</option>
+        <option value="3">Three</option>
+      </select>
+      <select className="form-select mb-3 w-25" aria-label="Month Select">
+        <option selected>Select Month</option>
+        <option value="1">One</option>
+        <option value="2">Two</option>
+        <option value="3">Three</option>
+      </select>
+      <button type="button" className="btn btn-danger">
+        Delete
+      </button>
+    </div>
+  );
 }
