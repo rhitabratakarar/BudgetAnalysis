2181e8e05983156ccf0d0b0e23802010d26fa6a7
Rhitabrata Karar <kararrhitabrata@gmail.com>
2024-10-26 20:24:05 +0530

    converted json to tsx

--- app-ui/src/config.tsx
+++ app-ui/src/config.tsx
@@ -1,6 +1,8 @@
-{
+let config = {
     "IsHTTPSEnabled": false,
     "HTTP_DbApiURL_Base": "http://localhost:5263",
     "HTTPS_DbApiURL_Base": "https://localhost:7168",
     "BudgetAnalysisApiController": "BudgetAnalysis"
-}
+};
+
+export default config;
