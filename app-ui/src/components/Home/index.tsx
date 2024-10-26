7f5633b8f22f9f0959391613971d4ce33bd56bfc
Rhitabrata Karar <kararrhitabrata@gmail.com>
2024-10-26 09:21:56 +0530

    Update index.tsx
    Content adjusted with flex-between

--- app-ui/src/components/Home/index.tsx
+++ app-ui/src/components/Home/index.tsx
@@ -2,7 +2,7 @@ import Card from "../Common/Card";
 
 export default function Home() {
   return (
-    <div className="container-fluid d-flex flex-row flex-wrap">
+    <div className="container-fluid d-flex flex-row flex-wrap justify-content-between">
       <Card />
       <Card />
       <Card />
