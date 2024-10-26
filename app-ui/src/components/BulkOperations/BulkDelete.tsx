a2aae71ad95cdb6062e6b9f842d5e6b6047882e0
Rhitabrata Karar <kararrhitabrata@gmail.com>
2024-10-26 09:43:12 +0530

    Update BulkDelete.tsx
    added shadow small to elements

--- app-ui/src/components/BulkOperations/BulkDelete.tsx
+++ app-ui/src/components/BulkOperations/BulkDelete.tsx
@@ -4,19 +4,19 @@ export default function BulkDelete() {
       <h6 className="mb-4">
         <strong>Note: </strong>This operation is irreversible.
       </h6>
-      <select className="form-select mb-3 w-25" aria-label="Year Select">
+      <select className="form-select mb-3 w-25 shadow-sm" aria-label="Year Select">
         <option selected>Select Year</option>
         <option value="1">One</option>
         <option value="2">Two</option>
         <option value="3">Three</option>
       </select>
-      <select className="form-select mb-3 w-25" aria-label="Month Select">
+      <select className="form-select mb-3 w-25 shadow-sm" aria-label="Month Select">
         <option selected>Select Month</option>
         <option value="1">One</option>
         <option value="2">Two</option>
         <option value="3">Three</option>
       </select>
-      <button type="button" className="btn btn-danger">
+      <button type="button" className="btn btn-danger shadow-sm">
         Delete
       </button>
     </div>
