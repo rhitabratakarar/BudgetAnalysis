4a0c784d5e44710c034fa6c976714946bdb0fda1
Rhitabrata Karar <kararrhitabrata@gmail.com>
2024-10-31 10:57:43 +0530

    Update Search/Index.tsx
    Included a table structure for the search page

--- app-ui/src/components/Search/index.tsx
+++ app-ui/src/components/Search/index.tsx
@@ -1,3 +1,40 @@
 export default function Search() {
-    return <div>Search Page</div>;
+  return (
+    <div className="container-fluid">
+      <table className="table table-bordered">
+        <thead>
+          <tr>
+            <th scope="col">Expense Type</th>
+            <th scope="col">Year</th>
+            <th scope="col">Month</th>
+            <th scope="col">Expense Name</th>
+            <th scope="col">Expense Cost</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Mandatory</td>
+            <td>2024</td>
+            <td>February</td>
+            <td>Some Expense Name</td>
+            <td>Some Expense Cost</td>
+          </tr>
+          <tr>
+            <td>Mandatory</td>
+            <td>2024</td>
+            <td>February</td>
+            <td>Some Expense Name</td>
+            <td>Some Expense Cost</td>
+          </tr>
+          <tr>
+            <td>Mandatory</td>
+            <td>2024</td>
+            <td>February</td>
+            <td>Some Expense Name</td>
+            <td>Some Expense Cost</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
 }
