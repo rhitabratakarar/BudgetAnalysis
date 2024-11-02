7ae57ff8379836f1fd8d7dbe6aab47ced169246a
Rhitabrata Karar <kararrhitabrata@gmail.com>
2024-11-02 22:05:57 +0530

    allowed cors

--- BudgetAnalysisDbApi/Program.cs
+++ BudgetAnalysisDbApi/Program.cs
@@ -11,6 +11,15 @@ builder.Configuration
 
 // Add services to the container.
 
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(
+        policy =>
+        {
+            policy.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod();
+        });
+});
+
 builder.Services.AddControllers();
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -33,6 +42,8 @@ if (app.Environment.IsDevelopment())
 }
 
 // app.UseHttpsRedirection();
+
+app.UseCors();
 
 app.UseAuthorization();
 
