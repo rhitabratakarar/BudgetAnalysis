5cc0aaa4ca81f09504ce04bfa693f19232597d8f
Rhitabrata Karar <kararrhitabrata@gmail.com>
2025-06-15 10:50:59 +0530

    Refactor (Backend): Sheet Sync Tool completely working with Google sheets
    In this commit, the sheet sync tool will work with Google Sheets to pull and save data into a locally given database. It does not require any db api microservice anymore.

--- SheetSyncTool/SheetSyncer.cs
+++ SheetSyncTool/SheetSyncer.cs
@@ -4,7 +4,9 @@ using Google.Apis.Auth.OAuth2;
 using Google.Apis.Sheets.v4;
 using Google.Apis.Sheets.v4.Data;
 using Google.Apis.Util.Store;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
 
 namespace SheetSyncTool
 {
@@ -86,9 +88,86 @@ namespace SheetSyncTool
         {
             try
             {
-                throw new NotImplementedException("SaveToDatabase Not implemented yet.");
+                #region insert year into database if it does not exists
+                int appConfigYearCode = Convert.ToInt32(this.configuration["SheetYear"])!;
+
+                Year? y = await this.dbDataAccess.DbContext.Years.Where(y => y.YearCode == appConfigYearCode).FirstOrDefaultAsync();
+
+                if (y == null)
+                {
+                    await this.dbDataAccess.DbContext.Years.AddAsync(new Year()
+                    {
+                        YearCode = appConfigYearCode
+                    });
+                }
+                #endregion
+
+                #region insert month into database if it does not exists
+                string appConfigMonthName = this.configuration["SheetName"]!;
+                Month? m = await this.dbDataAccess.DbContext.Months.Where(m => m.MonthName == appConfigMonthName).FirstOrDefaultAsync();
+
+                if (m == null)
+                {
+                    await this.dbDataAccess.DbContext.Months.AddAsync(new Month()
+                    {
+                        MonthName = appConfigMonthName
+                    });
+                }
+                #endregion
+
+                // save the changes according to month and year first. This is required since the ID columns are required in Expenses.
+                await this.dbDataAccess.DbContext.SaveChangesAsync();
+
+
+                #region Inserting Expenses
+                if (y == null)
+                    y = await this.dbDataAccess.DbContext.Years.Where(y => y.YearCode == appConfigYearCode).FirstOrDefaultAsync();
+                if (m == null)
+                    m = await this.dbDataAccess.DbContext.Months.Where(m => m.MonthName == appConfigMonthName).FirstOrDefaultAsync();
+
+                // skipping the first item (since they contents headers)
+                foreach (IList<object> row in contentToSave.Skip(1))
+                {
+                    // i, 0 => Mandatory Expense Type
+                    // i, 1 => Mandatory Expense Cost
+                    // i, 6 => Optional Expense Type
+                    // i, 7 => Optional Expense Cost
+
+
+                    // inserting Mandatory Expense if the row is not null or empty.
+                    if (!Convert.ToString(row[0]).IsNullOrEmpty() && !Convert.ToString(row[1]).IsNullOrEmpty())
+                    {
+                        await this.dbDataAccess.DbContext.Expenses.AddAsync(new Expense()
+                        {
+                            ExpenseType = 1,
+                            ExpenseName = Convert.ToString(row[0])!,
+                            ExpenseCost = Convert.ToDouble(row[1])!,
+                            CreatedDate = DateTime.Now,
+                            Month = m!,
+                            Year = y!
+                        });
+                    }
+
+                    // inserting Optional Expense if the row is not null or empty
+                    if (!Convert.ToString(row[6]).IsNullOrEmpty() && !Convert.ToString(row[7]).IsNullOrEmpty())
+                    {
+                        await this.dbDataAccess.DbContext.Expenses.AddAsync(new Expense()
+                        {
+                            ExpenseType = 2,
+                            ExpenseName = Convert.ToString(row[6])!,
+                            ExpenseCost = Convert.ToDouble(row[7])!,
+                            CreatedDate = DateTime.Now,
+                            Month = m!,
+                            Year = y!
+                        });
+                    }
+                }
+                #endregion
+
+                // save changes into database accordingly
+                await this.dbDataAccess.DbContext.SaveChangesAsync();
             }
-            catch(Exception e)
+            catch (Exception e)
             {
                 throw new Exception(e.Message);
             }
