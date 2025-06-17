910f30bf49536a561dd799e00d8ec1151a32bc23
Rhitabrata Karar <kararrhitabrata@gmail.com>
2025-06-17 13:42:15 +0530

    Feat (New pages): Added new pages.
    Pages such as yearly, weekly and monthly expenses have been added. Search and Export pages have also been added. These pages are not filled with logic yet.

---
+++ Website/Pages/MonthlyExpenditure.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Website.Pages
+{
+    public class MonthlyExpenditureModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
