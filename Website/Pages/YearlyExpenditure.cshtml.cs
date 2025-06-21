3261c493c852c630f1238acc24cc537cfbe27d74
Rhitabrata Karar <kararrhitabrata@gmail.com>
2025-06-21 10:30:15 +0530

    Feat (UI): Added select section in yearly expenditure page.
    In this commit, a multiple select have been added. This will feed data for further statistics.

--- Website/Pages/YearlyExpenditure.cshtml.cs
+++ Website/Pages/YearlyExpenditure.cshtml.cs
@@ -1,12 +1,40 @@
+using DataAccess;
+using DataAccess.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 
 namespace Website.Pages
 {
     public class YearlyExpenditureModel : PageModel
     {
-        public void OnGet()
+        [BindProperty]
+        public IList<string>? SelectedYears { get; set; }
+
+        public IList<Year>? ListOfYearsAvailableInDatabase { get; set; }
+
+
+        private readonly IDbDataAccess dbDataAccess;
+
+
+        public YearlyExpenditureModel(IDbDataAccess dbDataAccess)
         {
+            this.dbDataAccess = dbDataAccess;
+        }
+
+        public async Task OnGet()
+        {
+            IList<Year> yearsAvailable = await this.dbDataAccess.DbContext.Years.ToListAsync();
+            this.ListOfYearsAvailableInDatabase = yearsAvailable;
+        }
+
+        public async Task OnPost()
+        {
+            // refetch the list of years available in database
+            IList<Year> yearsAvailable = await this.dbDataAccess.DbContext.Years.ToListAsync();
+            this.ListOfYearsAvailableInDatabase = yearsAvailable;
+
+            // additional calculations
         }
     }
 }
