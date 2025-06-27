877e8af97a9cadc1e91e7a06c9b768a065c53caf
Rhitabrata Karar <kararrhitabrata@gmail.com>
2025-06-27 12:17:22 +0530

    Style (UI): Added Select tags in CleanUp page.
    In this commit, select elements have been added along with submit button to start with cleanup from the database.

--- Website/Pages/CleanUp.cshtml.cs
+++ Website/Pages/CleanUp.cshtml.cs
@@ -1,12 +1,37 @@
+using DataAccess;
+using DataAccess.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 
 namespace Website.Pages
 {
     public class CleanUpModel : PageModel
     {
-        public void OnGet()
+        public IList<Year>? ListOfYearsAvailableInDatabase { get; set; }
+        public IList<Month>? ListOfMonthsAvailableInDatabase { get; set; }
+
+        [BindProperty]
+        public IList<string>? SelectedYears { get; set; }
+        
+        [BindProperty]
+        public IList<string>? SelectedMonths { get; set; }
+
+
+        private readonly IDbDataAccess dbDataAccess;
+
+        public CleanUpModel(IDbDataAccess dbDataAccess)
         {
+            this.dbDataAccess = dbDataAccess;
+        }
+
+        public async Task OnGet()
+        {
+            IList<Year>? yearsAvailable = await this.dbDataAccess.DbContext.Years.ToListAsync();
+            this.ListOfYearsAvailableInDatabase = yearsAvailable;
+
+            IList<Month>? monthsAvailable = await this.dbDataAccess.DbContext.Months.ToListAsync();
+            this.ListOfMonthsAvailableInDatabase = monthsAvailable;
         }
     }
 }
