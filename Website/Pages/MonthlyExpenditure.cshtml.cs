eae7249cd7d1c8e4e03d8944378bf807dc4eac03
Rhitabrata Karar <kararrhitabrata@gmail.com>
2025-07-02 09:02:56 +0530

    Feat (UI): Added multi select in monthly expenses
    In this commit, monthly expenditure page have been updated and it was provided with 2 multiple selection sections to choose month and year from.

--- Website/Pages/MonthlyExpenditure.cshtml.cs
+++ Website/Pages/MonthlyExpenditure.cshtml.cs
@@ -1,12 +1,37 @@
+using DataAccess;
+using DataAccess.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 
 namespace Website.Pages
 {
     public class MonthlyExpenditureModel : PageModel
     {
-        public void OnGet()
+        public IList<Year>? ListOfYearsAvailableInDatabase { get; set; }
+        public IList<Month>? ListOfMonthsAvailableInDatabase { get; set; }
+
+
+        [BindProperty]
+        public IList<Year>? SelectedYears { get; set; }
+        [BindProperty]
+        public IList<Month>? SelectedMonths { get; set; }
+
+
+        private readonly IDbDataAccess dbDataAccess;
+
+        public MonthlyExpenditureModel(IDbDataAccess dbDataAccess)
         {
+            this.dbDataAccess = dbDataAccess;
+        }
+
+        public async Task OnGet()
+        {
+            IList<Year> yearsAvailable = await this.dbDataAccess.DbContext.Years.ToListAsync();
+            this.ListOfYearsAvailableInDatabase = yearsAvailable;
+
+            IList<Month> monthsAvailable = await this.dbDataAccess.DbContext.Months.ToListAsync();
+            this.ListOfMonthsAvailableInDatabase = monthsAvailable;
         }
     }
 }
