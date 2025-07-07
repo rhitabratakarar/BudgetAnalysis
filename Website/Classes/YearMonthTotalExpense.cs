346216fab0360bbd14efc4d23051422c255a32b7
Rhitabrata Karar <kararrhitabrata@gmail.com>
2025-07-07 09:30:45 +0530

    Feat (UI): Functionality to show summation of expenditures in monthly expenditures page.
    In this commit, the MonthlyExpenditure.cshtml and its .cs file have been modified to show the total expenditure based on year and month. New DTO classes have been added to map the data back to the frontend. Appsettings will have a minute change based on data import in sheet sync tool (not important).

---
+++ Website/Classes/YearMonthTotalExpense.cs
@@ -0,0 +1,11 @@
+﻿using DataAccess.Models;
+
+namespace Website.Classes
+{
+    public class YearMonthTotalExpense
+    {
+        public int? YearCode { get; set; }
+        public string? MonthName { get; set; }
+        public double TotalExpense { get; set; } = 0.0;
+    }
+}
--- Website/Pages/MonthlyExpenditure.cshtml.cs
+++ Website/Pages/MonthlyExpenditure.cshtml.cs
@@ -3,6 +3,7 @@ using DataAccess.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
+using Website.Classes;
 
 namespace Website.Pages
 {
@@ -11,11 +12,14 @@ namespace Website.Pages
         public IList<Year>? ListOfYearsAvailableInDatabase { get; set; }
         public IList<Month>? ListOfMonthsAvailableInDatabase { get; set; }
 
+        public IList<YearMonthTotalExpense>? YearMonthTotalExpense { get; set; }
+
 
         [BindProperty]
-        public IList<Year>? SelectedYears { get; set; }
+        public IList<string>? SelectedYears { get; set; }
+
         [BindProperty]
-        public IList<Month>? SelectedMonths { get; set; }
+        public IList<string>? SelectedMonths { get; set; }
 
 
         private readonly IDbDataAccess dbDataAccess;
@@ -32,6 +36,50 @@ namespace Website.Pages
 
             IList<Month> monthsAvailable = await this.dbDataAccess.DbContext.Months.ToListAsync();
             this.ListOfMonthsAvailableInDatabase = monthsAvailable;
+        }
+
+        public async Task OnPost()
+        {
+            // reassign available years
+            IList<Year> yearsAvailable = await this.dbDataAccess.DbContext.Years.ToListAsync();
+            this.ListOfYearsAvailableInDatabase = yearsAvailable;
+
+            // reassign available months
+            IList<Month> monthsAvailable = await this.dbDataAccess.DbContext.Months.ToListAsync();
+            this.ListOfMonthsAvailableInDatabase = monthsAvailable;
+
+            await CalculateTotalMonthlyExpenditures();
+        }
+
+        public async Task CalculateTotalMonthlyExpenditures()
+        {
+            this.YearMonthTotalExpense = new List<YearMonthTotalExpense>();
+
+            // based on selected year and month, calculate the total expenses on a month basis.
+            if (this.SelectedYears != null && this.SelectedMonths != null && this.SelectedYears.Any() && this.SelectedMonths.Any())
+            {
+                foreach (string year in this.SelectedYears)
+                {
+                    foreach(string month in this.SelectedMonths)
+                    {
+                        if (year != "select" && month != "select")
+                        {
+                            IList<Expense> e = await this.dbDataAccess.DbContext.Expenses
+                                                .Include(e => e.Year)
+                                                .Include(e => e.Month)
+                                                .Where(e => e.Year.YearCode == Convert.ToInt32(year) && e.Month.MonthName == month)
+                                                .ToListAsync();
+
+                            this.YearMonthTotalExpense.Add(new YearMonthTotalExpense()
+                            {
+                                YearCode = Convert.ToInt32(year),
+                                MonthName = month,
+                                TotalExpense = e.Sum(s => s.ExpenseCost)
+                            });
+                        }
+                    }
+                }
+            }
         }
     }
 }
